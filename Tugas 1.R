17346bca8db9854ba1191e5191c1ded770b76bf0
17523229 <43648020+17523229@users.noreply.github.com>
2018-09-27 22:35:19 +0700

    Add files via upload
    Tugas Pemrograman R - Fungsi

---
+++ Tugas 1.R
@@ -0,0 +1,41 @@
+#Exercise
+
+#Constant Functions
+
+f1 <- function(x){
+  result <- 5
+  return(result)
+}
+f1(2)
+
+#Linear Functions
+
+f2 <- function(x){
+  result <- 2*x+2
+  return(result)
+}
+f2(2)
+
+#Quadratic Functions
+
+f3 <- function(x){
+  result <- x^2-4*x+4
+  return(result)
+}
+f3(0)
+
+#Polynomial Functions
+
+f4 <- function(x){
+  result <- x^4+x^3+x^2+x+2
+  return(result)
+}
+f4(1)
+
+#Rational Functions
+
+f5 <- function(x){
+  result <- 2*x+1/x+3
+  return(result)
+}
+f5(1)
